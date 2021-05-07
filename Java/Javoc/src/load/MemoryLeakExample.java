3e305966b287fa70299f6cbbfbeaf4589883a3b0
Lulmao <yxchia98@gmail.com>
2021-05-07 19:41:28 +0800

    update

--- Java/Javoc/src/load/MemoryLeakExample.java
+++ Java/Javoc/src/load/MemoryLeakExample.java
@@ -1,23 +1,35 @@
 package load;
 
-import java.util.Vector;
+import java.util.ArrayList;
+import java.lang.management.*;
 
 public class MemoryLeakExample {
 	public static void main(String[] args) throws InterruptedException {
-		double totalmemory = Runtime.getRuntime().totalMemory();
-		double availablememory = totalmemory - Runtime.getRuntime().freeMemory();
-		double maxmemory = Runtime.getRuntime().maxMemory();
-		System.out.println("Total memory: " + totalmemory / Math.pow(10, 9));
-		System.out.println("Available memory: " + availablememory / Math.pow(10, 9));
-		System.out.println("Max memory: " + maxmemory / Math.pow(10, 9) + "\n\n");
-
-		Vector v1 = new Vector(1000000000);
-		totalmemory = Runtime.getRuntime().totalMemory();
-		availablememory = totalmemory - Runtime.getRuntime().freeMemory();
-		maxmemory = Runtime.getRuntime().maxMemory();
-		System.out.println("Total memory: " + totalmemory / Math.pow(10, 9));
-		System.out.println("Available memory: " + availablememory / Math.pow(10, 9));
-		System.out.println("Max memory: " + maxmemory / Math.pow(10, 9) + "\n\n");
+		com.sun.management.OperatingSystemMXBean os = (com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+		System.out.println(os.getAvailableProcessors());
+		System.out.println(os.getTotalMemorySize());
+		System.out.println(os.getFreeMemorySize());
+		System.out.println("Total memory: " + Runtime.getRuntime().totalMemory() / Math.pow(2, 30) + " GB, " + (long)Runtime.getRuntime().totalMemory() + " B.");
+		System.out.println("Used memory: " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / Math.pow(2, 30) + " GB, " + (long)(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) + " B.");
+		System.out.println("Max memory: " + Runtime.getRuntime().maxMemory() / Math.pow(2, 30) + " GB, " + (long)Runtime.getRuntime().maxMemory() + " B.");
+		
+		double inputsize = 4096 * Math.pow(2, 20);
+		System.out.println(inputsize);
+		ArrayList<char[]> hog = new ArrayList<char[]>();
+		Runtime.getRuntime().gc();
+		while((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) < inputsize) {
+			hog.add(new char[524288]);
+		}
+//		for(int i = 0; i < inputsize; i++) {
+////			hog.add(new char[524288]);
+//			hog.add(new char[500000]);
+//		}
+		System.out.println();
+		System.out.println(os.getTotalMemorySize());
+		System.out.println(os.getFreeMemorySize());
+		System.out.println("Total memory: " + Runtime.getRuntime().totalMemory() / Math.pow(2, 30) + " GB, " + (long)Runtime.getRuntime().totalMemory() + " B.");
+		System.out.println("Used memory: " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / Math.pow(2, 30) + " GB, " + (long)(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) + " B.");
+		System.out.println("Max memory: " + Runtime.getRuntime().maxMemory() / Math.pow(2, 30) + " GB, " + (long)Runtime.getRuntime().maxMemory() + " B." + "\n\n");
 		Thread.sleep(10000);
 		System.out.println("There is no memory leak in this program.");
 	}
