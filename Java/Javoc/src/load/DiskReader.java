13fddd6874b1f21b9f052e5b8df1534a41f72fab
Lulmao <yxchia98@gmail.com>
2021-06-23 11:22:32 +0800

    delete unused files

--- Java/Javoc/src/load/DiskReader.java
+++
@@ -1,5 +0,0 @@
-package load;
-
-
-public class DiskReader {
-}
--- Java/Javoc/src/load/NetworkLagger.java
+++
@@ -1,7 +0,0 @@
-package load;
-
-public class NetworkLagger {
-	public static void main(String[] args) {
-		System.out.println(System.getProperty("os.name"));
-	}
-}
