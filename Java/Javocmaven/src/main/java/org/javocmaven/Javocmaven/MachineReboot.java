e955c3ea7a402e214a48b7f525c33f68af71e6ba
Lulmao <yxchia98@gmail.com>
2021-06-23 11:03:05 +0800

    update

--- Java/Javocmaven/src/main/java/org/javocmaven/Javocmaven/MachineReboot.java
+++ Java/Javocmaven/src/main/java/org/javocmaven/Javocmaven/MachineReboot.java
@@ -1,6 +1,8 @@
 package org.javocmaven.Javocmaven;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MachineReboot extends Loader{
 	
@@ -52,6 +54,25 @@ public class MachineReboot extends Loader{
 		} catch (IOException | InterruptedException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	protected void execCommand(ProcessBuilder builder) throws IOException {
+		builder.redirectErrorStream(true);
+		Process p = builder.start();
+		p.getOutputStream().close();
+		String line;
+		// Standard Output
+		BufferedReader stdout = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		while ((line = stdout.readLine()) != null) {
+			System.out.println(line);
+		}
+		stdout.close();
+		// Standard Error
+		BufferedReader stderr = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+		while ((line = stderr.readLine()) != null) {
+		}
+		stderr.close();
+		p.destroy();
 	}
 	
 }
