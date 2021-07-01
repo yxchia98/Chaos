e90514207bb4cbecce237780e4a550e9d4e83371
Lulmao <yxchia98@gmail.com>
2021-07-01 15:44:05 +0800

    Added export to JSON, CSV

--- Java/Javocmaven/src/main/java/org/javocmaven/Javocmaven/Logger.java
+++ Java/Javocmaven/src/main/java/org/javocmaven/Javocmaven/Logger.java
@@ -3,68 +3,179 @@ package org.javocmaven.Javocmaven;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.management.ManagementFactory;
 import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.TimerTask;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.opencsv.CSVWriter;
+
 public class Logger extends TimerTask {
+	private String date, time, cpuload = " ", os, totalmem_string, usedmem_string, usedpercentmem_string,
+			totalspace_string, usedspace_string, usedpercentdisk_string;
+
 	public void run() {
 		File disklog = new File("/");
 		Timestamp ts = new Timestamp(System.currentTimeMillis());
-		String logtxt = "LOGGED ON " + ts + "\n";
+
+		Date datetime = new Date(ts.getTime());
+		DateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
+		DateFormat timeformat = new SimpleDateFormat("HH:mm:ss");
+		this.date = dateformat.format(datetime);
+		this.time = timeformat.format(datetime);
 
 		long totalspace = disklog.getTotalSpace();
 		long freespace = disklog.getUsableSpace();
 		long usedspace = totalspace - freespace;
 		double usedpercentdisk = (double) usedspace / totalspace * 100;
+		this.totalspace_string = String.valueOf(Math.toIntExact((long) (totalspace / Math.pow(2, 20))));
+		this.usedspace_string = String.valueOf(Math.toIntExact((long) (usedspace / Math.pow(2, 20))));
+		this.usedpercentdisk_string = String.valueOf(Math.round(usedpercentdisk));
 
 		com.sun.management.OperatingSystemMXBean oslog = (com.sun.management.OperatingSystemMXBean) ManagementFactory
 				.getOperatingSystemMXBean();
-		String os = System.getProperty("os.name");
 
 		long totalmem = oslog.getTotalMemorySize();
 		long freemem = oslog.getFreeMemorySize();
 		long usedmem = totalmem - freemem;
 //		long jvmmem = Runtime.getRuntime().maxMemory();
 		double usedpercentmem = (double) usedmem / totalmem * 100;
+		this.totalmem_string = String.valueOf(Math.toIntExact((long) (totalmem / Math.pow(2, 20))));
+		this.usedmem_string = String.valueOf(Math.toIntExact((long) (usedmem / Math.pow(2, 20))));
+		this.usedpercentmem_string = String.valueOf(Math.round(usedpercentmem));
 
-//		System.out.println("LOGGED ON " + ts);
+		this.os = System.getProperty("os.name");
 
-		logtxt += "Platform: " + os + "\n";
-
-		if (os.contains("Windows")) {
+		if (this.os.contains("Windows")) {
 			try {
-				logtxt += "CPU load: " + getCPU("Windows") + "\n";
+				this.cpuload = getCPU("Windows");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		} else {
 			try {
-				logtxt += "CPU load: " + getCPU("Linux") + "\n";
+				this.cpuload = getCPU("Linux");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		logtxt += "Total Memory(100%): " + Math.toIntExact((long) (totalmem / Math.pow(2, 20))) + "MB\nUsed Memory("
-				+ Math.round(usedpercentmem) + "%): " + Math.toIntExact((long) (usedmem / Math.pow(2, 20))) + "MB\n";
-		
+		exportTXT();
+		exportCSV();
+		exportJSON();
+
+	}
+
+	private void exportCSV() {
+
+		List<String[]> csvData = new ArrayList<>();
+
+		String[] csvLog = { this.date, this.time, this.os, this.cpuload, this.totalmem_string, this.usedmem_string,
+				this.usedpercentmem_string, this.totalspace_string, this.usedspace_string,
+				this.usedpercentdisk_string };
+		if (new File("javoclog.csv").exists()) {
+			// append to file
+			try {
+				csvData.add(csvLog);
+				CSVWriter writer = new CSVWriter(new FileWriter("javoclog.csv", true));
+				writer.writeAll(csvData, false);
+				writer.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				String[] header = { "Log Date", "Log Time", "Platform", "CPU Load(%)", "Total Memory(MB)",
+						"Used Memory(MB)", "Used Memory(%)", "Total Space(MB)", "Used Space(MB)", "Used Space(%)" };
+				csvData.add(header);
+				csvData.add(csvLog);
+				CSVWriter writer = new CSVWriter(new FileWriter("javoclog.csv"));
+				writer.writeAll(csvData, false);
+				writer.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	private void exportJSON() {
+
+		LinkedHashMap<String, String> valuesmap = new LinkedHashMap<>();
+
+		valuesmap.put("Log Date", this.date);
+		valuesmap.put("Log Time", this.time);
+		valuesmap.put("Platform", this.os);
+		valuesmap.put("CPU Load(%)", this.cpuload);
+		valuesmap.put("Total Memory(MB)", this.totalmem_string);
+		valuesmap.put("Used Memory(MB)", this.usedmem_string);
+		valuesmap.put("Used Memory(%)", this.usedpercentmem_string);
+		valuesmap.put("Total Disk(MB)", this.totalspace_string);
+		valuesmap.put("Used Disk(MB)", this.usedspace_string);
+		valuesmap.put("Used Disk(%)", this.usedpercentdisk_string);
+		GsonBuilder builder = new GsonBuilder();
+		builder.setPrettyPrinting();
+		String jsonData = builder.create().toJson(valuesmap);
+
+		try {
+			FileWriter writer = new FileWriter("javoclog.json", true);
+			writer.write(jsonData);
+			writer.write(System.getProperty("line.separator"));
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+//		File javoclog = new File("javoc_log.json");
+//		FileOutputStream fos;
+//		try {
+//			fos = new FileOutputStream(javoclog, true);
+//			fos.write(jsonData.getBytes());
+//			fos.close();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+
+	}
+
+	private void exportTXT() {
+
+		String logtxt = "LOG DATE: " + this.date + "\n";
+		logtxt += "LOG TIME: " + this.time + "\n";
+
+		logtxt += "Platform: " + this.os + "\n";
+
+		logtxt += "CPU load: " + this.cpuload + "%\n";
+
+		logtxt += "Total Memory(100%): " + this.totalmem_string + "MB\nUsed Memory(" + this.usedpercentmem_string
+				+ "%): " + this.usedmem_string + "MB\n";
+
 //		logtxt += "JVM Allocated Memory: " + jvmmem / Math.pow(2, 20) + "MB\n";
 
-		logtxt += "Total Space(100%): " + Math.toIntExact((long) (totalspace / Math.pow(2, 20))) + "MB\nUsed Space("
-				+ Math.round(usedpercentdisk) + "%): " + Math.toIntExact((long) (usedspace / Math.pow(2, 20)))
-				+ "MB\n\n";
+		logtxt += "Total Space(100%): " + this.totalspace_string + "MB\nUsed Space(" + this.usedpercentdisk_string
+				+ "%): " + this.usedspace_string + "MB\n\n";
 
-		File javoclog = new File("javoc.log");
+		File javoclog = new File("javoclog.log");
 		FileOutputStream fos;
 		try {
 			fos = new FileOutputStream(javoclog, true);
 			fos.write(logtxt.getBytes());
+			fos.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 	private String getCPU(String type) throws IOException {
@@ -80,7 +191,7 @@ public class Logger extends TimerTask {
 			BufferedReader stdout = new BufferedReader(new InputStreamReader(p.getInputStream()));
 			while ((line = stdout.readLine()) != null) {
 				stdout.close();
-				return line + "%";
+				return line;
 			}
 			stdout.close();
 			// Standard Error
@@ -91,7 +202,9 @@ public class Logger extends TimerTask {
 			}
 		} else {
 			builder = new ProcessBuilder("bash", "-c",
-					"top -bn1 | grep -Po \"[0-9.]*(?=( id,))\" | awk '{print 100 - $1\"%\"}'");
+					"top -bn1 | grep -Po \"[0-9.]*(?=( id,))\" | awk '{print 100 - $1}'");
+//			builder = new ProcessBuilder("bash", "-c",
+//					"top -bn1 | grep -Po \"[0-9.]*(?=( id,))\" | awk '{print 100 - $1\"%\"}'");
 			builder.redirectErrorStream(true);
 			Process p = builder.start();
 			p.getOutputStream().close();
