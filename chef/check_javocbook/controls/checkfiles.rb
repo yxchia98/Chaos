98cd0b5f192e4f69835ea04deff35430e061f58f
Lulmao <yxchia98@gmail.com>
2021-07-15 11:18:04 +0800

    update

--- chef/check_javocbook/controls/checkfiles.rb
+++ chef/check_javocbook/controls/checkfiles.rb
@@ -2,9 +2,6 @@ control 'checkjavocfiles' do
   impact 1.0
   title 'Check Javocbook files'
   desc 'Check for Javocbook files and directories on target machine.'
-  describe directory('jdk-16.0.1') do
-    it { should exist }
-  end
   describe file('Javoc.jar') do
     it { should exist }
   end
